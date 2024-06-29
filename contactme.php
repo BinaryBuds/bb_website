94dcc39f369cf3db0ba6adaaf0abe5c10a9ae544
Himanshu <himanshu.budhiraja4@gmail.com>
2024-06-29 17:28:35 +0530

    Content Update

--- contactme.php
+++ contactme.php
@@ -1,16 +1,34 @@
-<?php   
-    require("./mailing/mailfunction.php");
+<?php
+// Assuming your mailfunction.php defines a function like mailfunction($to, $subject, $body)
+require("./mailing/mailfunction.php");
 
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+    // Collect form data
     $name = $_POST["name"];
-    $phone = $_POST['phone'];
+    $phone = $_POST["phone"];
     $email = $_POST["email"];
     $message = $_POST["message"];
 
-    $body = "<ul><li>Name: ".$name."</li><li>Phone: ".$phone."</li><li>Email: ".$email."</li><li>Message: ".$message."</li></ul>";
+    // Prepare email body
+    $body = "<ul>
+                <li>Name: ".$name."</li>
+                <li>Phone: ".$phone."</li>
+                <li>Email: ".$email."</li>
+                <li>Message: ".$message."</li>
+            </ul>";
 
-    $status = mailfunction("", "Company", $body); //reciever
-    if($status)
+    // Replace with the appropriate recipient email address
+    $recipient = "binarybuds5@gmail.com"; // Replace with your recipient's email address
+
+    // Send email using your custom function
+    $status = mailfunction($recipient, "New Contact Form Submission", $body);
+
+    if ($status) {
         echo '<center><h1>Thanks! We will contact you soon.</h1></center>';
-    else
-        echo '<center><h1>Error sending message! Please try again.</h1></center>';    
+    } else {
+        echo '<center><h1>Error sending message! Please try again.</h1></center>';
+    }
+} else {
+    echo '<center><h1>Error: Form submission method not allowed.</h1></center>';
+}
 ?>
--- js/main.js
+++ js/main.js
@@ -20,12 +20,13 @@ $(document).ready(function(){
     });
 
     const counters = document.querySelectorAll('.counter');
-    const speed = 120;
+    const speed = 100;
     counters.forEach(counter => {
 	const updateCount = () => {
 		const target = +counter.getAttribute('data-target');
 		const count = +counter.innerText;
-		const inc = target / speed;
+        debugger;
+		const inc = Math.round(target / speed) + 1;
 		if (count < target) {
 			counter.innerText = count + inc;
 			setTimeout(updateCount, 1);
